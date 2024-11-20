53ec44ca0dafbfae1d978ddf87c6140c155e77a7
Nicolas Lell <nicolell390@gmail.com>
2024-11-20 17:49:29 -0300

    Agregamos las seed de product y category usando faker

---
+++ seed/categorySeed.js
@@ -0,0 +1,25 @@
+import { Category } from "../models/models.js";
+import faker from "faker"; 
+
+async function categorySeed() {
+  try {
+    // Generar categorías falsas usando faker
+    const categories = [];
+    for (let i = 0; i < 5; i++) { 
+      categories.push({
+        category_name: faker.commerce.department()
+      });
+    }
+
+    // Insertar las categorías en la base de datos
+    await Category.bulkCreate(categories, {
+      individualHooks: true 
+    });
+
+    console.log('Categorías insertadas correctamente');
+  } catch (error) {
+    console.error("Error al insertar categorías:", error);
+  }
+}
+
+export default categorySeed;
---
+++ seed/productSeed.js
@@ -0,0 +1,27 @@
+import { Product } from "../models/models.js";
+import faker from "faker"; 
+
+async function productSeed() {
+  try {
+    const products = [];
+    
+    for (let i = 0; i < 10; i++) {
+      products.push({
+        product_name: faker.commerce.productName(),
+        price: parseFloat(faker.commerce.price()),  
+        categoryID: faker.datatype.uuid(),  
+      });
+    }
+
+    // Insertar los productos generados en la base de datos
+    await Product.bulkCreate(products, {
+      individualHooks: true
+    });
+
+    console.log('Productos insertados correctamente');
+  } catch (error) {
+    console.error("Error al insertar productos:", error);
+  }
+}
+
+export default productSeed;
