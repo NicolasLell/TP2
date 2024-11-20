0b0b8cd0c95f512f68e2a15443ab778bb15e914b
Nicolas Lell <nicolell390@gmail.com>
2024-11-20 19:30:03 -0300

    Solucionando errores en el index con el flujo de los datos

--- index.js
+++ index.js
@@ -4,22 +4,35 @@ import connection from "./connection/connection.js";
 import { SERVER_PORT } from "./config/config.js";
 import productSeed from "./seed/productSeed.js";
 import categorySeed from "./seed/categorySeed.js";
-import routes from "./router/index.js"
-
+import routes from "./router/index.js";
 
 const app = express();
-const port = SERVER_PORT || 3000
+const port = SERVER_PORT || 3000;
 
+// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Rutas
+app.use(routes);  
+
+// Seed y conexión a la base de datos
+const startServer = async () => {
+  try {
+    await connection.sync();  
+    await productSeed();       
+    await categorySeed();      
+    
+    
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Error during server startup:", error);
+  }
+};
+
+startServer();
+
+
 app.use(notFound);
-app.use(routes);
-
-await connection.sync();
-await productSeed();
-await categorySeed();
-
-app.listen(port,()=>{
-    console.log('Server en el puerto 3000')
-})
